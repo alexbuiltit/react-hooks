ed650d7b29e207dca94a3b95af95b4b8ef4a2324
Alex Turner <hello@alexbuilt.it>
2020-01-24 23:51:05 +0000

    fix: accordion modifications

--- components/features/accordion/Accordion.js
+++ components/features/accordion/Accordion.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const Accordion = ({ title, children }) => {
+const Accordion = ({ title, uniqueId, children }) => {
   const [open, setOpen] = useState(false);
   const toggleAccordion = () => {
     setOpen(!open);
@@ -11,15 +11,19 @@ const Accordion = ({ title, children }) => {
     <div className="accordion__item">
       <button
         className="accordion__title"
+        id={`${uniqueId}title`}
         onClick={toggleAccordion}
         aria-expanded={open}
+        aria-controls={uniqueId}
       >
         {title}
       </button>
       {open && (
         <div
           className={`accordion__content ${open ? 'open' : 'closed'}`}
+          id={uniqueId}
           role="region"
+          aria-labelledby={`${uniqueId}title`}
         >
           {children}
         </div>
@@ -29,7 +33,8 @@ const Accordion = ({ title, children }) => {
 };
 
 Accordion.propTypes = {
-  title: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  uniqueId: PropTypes.string.isRequired,
   children: PropTypes.node,
 };
 
--- pages/accordion.js
+++ pages/accordion.js
@@ -4,7 +4,7 @@ import Accordion from '../components/features/accordion/Accordion';
 const Index = () => {
   return (
     <Layout>
-      <Accordion title="Accordion 1">
+      <Accordion title="Accordion 1" uniqueId="firstaccordion">
         <p>
           Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
           Nulla vitae elit libero, a pharetra augue. Nulla vitae elit libero, a
@@ -13,7 +13,7 @@ const Index = () => {
           euismod. Donec ullamcorper nulla non metus auctor fringilla.
         </p>
       </Accordion>
-      <Accordion title="Accordion 2">
+      <Accordion title="Accordion 2" uniqueId="secondaccordion">
         <p>
           Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
           Nulla vitae elit libero, a pharetra augue. Nulla vitae elit libero, a
